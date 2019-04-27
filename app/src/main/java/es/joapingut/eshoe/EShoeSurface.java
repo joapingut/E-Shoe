202da45bbdd56a0fa9c74aa92958670d1a6cc24c
Joaquin Pineda Gutierrez <joaquin_ret@hotmail.com>
2019-04-27 17:58:27 +0200

    Revert "Merge branch 'simple_protocol' of https://github.com/joapingut/E-Shoe into reverse_results"
    This reverts commit 03a873f3aae7b4e50b93887fbfa286673b7ad528, reversing
    changes made to 9296dc3f80ab65b3228692a2b9556cb9dc6526d9.

--- app/src/main/java/es/joapingut/eshoe/EShoeSurface.java
+++ app/src/main/java/es/joapingut/eshoe/EShoeSurface.java
@@ -191,9 +191,9 @@ public class EShoeSurface implements SurfaceHolder.Callback {
             g = 255;
             b = 255 - (int)(255 * percent);
         } else {
-            percent = (force - 0.5F) * 2;
+            percent = EShoeUtils.extrapolate(force, 0.5F,1F,0F,1F);
             r = 255;
-            g = 255 - (int)(255 * (percent > 1.0F ? 1.0F : percent));
+            g = 255 - (int)(255 * percent);
             b = 0;
         }
 
